daa24e7ca9184edf5cea9017407b7314bc94c1a7
youngyin <57608725+youngyin@users.noreply.github.com>
2025-03-16 18:47:04 +0900

    [feat] init project

---
+++ src/test/kotlin/sdv/moviebook/MovieBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package sdv.moviebook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieBookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
