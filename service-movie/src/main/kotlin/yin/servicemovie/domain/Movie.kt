8d25180e2d6cd88ffec5170f88dd23ea4002d3e2
youngyin <57608725+youngyin@users.noreply.github.com>
2025-03-17 07:47:17 +0900

    [fix] 필요없는 설정 제거

--- service-movie/src/main/kotlin/yin/servicemovie/domain/Movie.kt
+++ service-movie/src/main/kotlin/yin/servicemovie/domain/Movie.kt
@@ -1,12 +1,7 @@
 package yin.servicemovie.domain
 
-import lombok.AccessLevel
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.time.LocalDate
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 data class Movie(
     val id: Long,
     val title: String,
