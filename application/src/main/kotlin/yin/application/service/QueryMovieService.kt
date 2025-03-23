98bea1b2add497bdeaa647b6a9d4da7a71970e75
youngyin <57608725+youngyin@users.noreply.github.com>
2025-03-24 07:34:18 +0900

    [fix] 트랜잭션 설정

--- application/src/main/kotlin/yin/application/service/QueryMovieService.kt
+++ application/src/main/kotlin/yin/application/service/QueryMovieService.kt
@@ -3,11 +3,13 @@ package yin.application.service
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import yin.application.command.QueryMovieCommand
 import yin.application.dto.QueryMovieResponse
 import yin.application.port.`in`.QueryMovieUseCase
 import yin.application.port.out.MovieRepositoryPort
 
+@Transactional(readOnly = true)
 @Service
 class QueryMovieService(
     private val movieRepositoryPort: MovieRepositoryPort
