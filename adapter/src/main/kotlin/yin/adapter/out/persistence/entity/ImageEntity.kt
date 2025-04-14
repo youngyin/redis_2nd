5b73bc434a26426af18cc3541d376706a95ded00
youngyin <57608725+youngyin@users.noreply.github.com>
2025-04-15 08:00:25 +0900

    [docs] 주석

--- adapter/src/main/kotlin/yin/adapter/out/persistence/entity/ImageEntity.kt
+++ adapter/src/main/kotlin/yin/adapter/out/persistence/entity/ImageEntity.kt
@@ -2,6 +2,11 @@ package yin.adapter.out.persistence.entity
 
 import jakarta.persistence.*
 
+/**
+ * For example, on a detail page,
+ * multiple images such as still cuts showing various angles or scenes,and teaser posters may be required.
+ * Considering this scalability, the images table has been separated.
+ */
 @Entity
 @Table(name = "images")
 open class ImageEntity public constructor(
